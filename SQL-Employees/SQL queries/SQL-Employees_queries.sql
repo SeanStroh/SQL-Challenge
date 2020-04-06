cc56ce54b905a5450eaf27448442bb9ca471fd18
SeanStroh <57781525+SeanStroh@users.noreply.github.com>
2020-04-05 22:01:44 -0600

    this is the last hopefully

--- SQL-Employees/SQL queries/SQL-Employees_queries.sql
+++ SQL-Employees/SQL queries/SQL-Employees_queries.sql
@@ -17,3 +17,39 @@ from "dept_manager" a
 join "departments" b on (a.dept_no = b.dept_no)
 join "employees" c on (a.emp_no = c.emp_no);
 
+-- question 4 List the department of each employee with the following information:
+--  employee number, last name, first name, and department name.
+select a.emp_no, a.first_name, a.last_name, c.dept_name
+from "employees" a 
+join "dept_emp" b on (a.emp_no = b.emp_no)
+join "departments" c on (b.dept_no = c.dept_no);
+
+-- question 5 List all employees whose first name is "Hercules" and last names begin with "B."
+select a.first_name, a.last_name
+from "employees" a 
+where first_name = 'Hercules'
+and last_name like ('B%');
+
+-- question 6 List all employees in the Sales department, including 
+-- their employee number, last name, first name, and department name.
+select a.emp_no, a.first_name, a.last_name, c.dept_name
+from "employees" a 
+join "dept_emp" b on (a.emp_no = b.emp_no)
+join "departments" c on (b.dept_no = c.dept_no)
+where dept_name = 'Sales';
+
+-- question 7 List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+select a.emp_no, a.first_name, a.last_name, c.dept_name
+from "employees" a 
+join "dept_emp" b on (a.emp_no = b.emp_no)
+join "departments" c on (b.dept_no = c.dept_no)
+where dept_name = 'Sales'
+or dept_name = 'Development';
+
+-- question 8 In descending order, list the frequency count of
+-- employee last names, i.e., how many employees share each last name.
+SELECT last_name, count(*)
+FROM "employees"
+group by last_name
+order by count DESC;
